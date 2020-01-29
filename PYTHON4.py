aa5fe6a95219a68cc4a582692e2cbce6b785b75a
jesus02-06 <60182911+jesus02-06@users.noreply.github.com>
2020-01-29 09:32:33 -0400

    Ejercicio

---
+++ PYTHON4.py
@@ -0,0 +1,14 @@
+divisor = -1
+dividendo = -1
+
+while(dividendo <= 0 or divisor <= 0):
+    dividendo = int(input('dijite el dividendo'))
+    divisor = int(input('dijite el divisor'))
+
+    if(dividendo <= 0 or divisor <= 0):
+        print('Error, solo positivos.')
+
+while(dividendo >= divisor):
+    dividendo -= divisor
+
+print(dividendo)
